0febd57ae9d8a7140fb8e3881906895330d85fce
unknown <erick_alessi@outlook.com>
2023-03-12 23:35:44 +0000

    pag 206

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,58 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour{
     [SerializeField] private float movementSpeed;
-    [SerializeField] private bool movementActive = true;
-
     [SerializeField] private float rotationSpeed;
-    
+    [SerializeField] private bool movementActive = true;
     [SerializeField] private bool rotationXActive = true;
-    [SerializeField] private bool rotationYActive = true;
+    [SerializeField] private bool rotationYActive = false;
 
-    private float totalXRot = 0f;
+    private Vector2 movementValue;
+    private Vector2 lookValue;
+    private float totalXRotation = 0f;
+    private float totalYRotation = 0f;
 
-    private float totalYRot = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
+    // Update is called once per frame
+
+    private void Awake(){
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        var normalizedSpeed = movementSpeed * Time.deltaTime;
-        var normalizedRotation = rotationSpeed * Time.deltaTime;
+    public void OnLook(InputValue value){
+        lookValue = value.Get<Vector2>() * rotationSpeed;
+    }
 
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
-            transform.Translate(0,0,normalizedSpeed);
-        }
-        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
-            transform.Translate(0,0,-normalizedSpeed);
-        }
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(-normalizedSpeed,0,0);
-        }
-        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(normalizedSpeed,0,0);
+    public void OnMove(InputValue value){
+        movementValue = value.Get<Vector2>() * movementSpeed;
+    }
+
+    private void Update(){
+        if (movementActive){
+            transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
         }
 
-        if (rotationXActive)
-        {
-            var mouseX = Input.GetAxis("Mouse X");
-            totalXRot += mouseX * normalizedRotation;
-        }
-        
-        if (rotationYActive)
-        {
-            var mouseY = Input.GetAxis("Mouse Y");
-            totalYRot += -mouseY * normalizedRotation;
-        }
-        transform.rotation = Quaternion.Euler(totalYRot, totalXRot, 0f);
 
+        if (rotationXActive){
+            totalXRotation += lookValue.x * Time.deltaTime;
+        }
+
+        if (rotationYActive){
+            totalYRotation += -lookValue.y * Time.deltaTime;
+        }
+
+        transform.rotation = Quaternion.Euler(totalYRotation, totalXRotation, 0);
     }
 }
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -2,26 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
-public class PlayerShooting : MonoBehaviour
-{
+public class PlayerShooting : MonoBehaviour{
     [SerializeField] private GameObject bullet;
     [SerializeField] private GameObject shootPoint;
 
     // Start is called before the first frame update
-    void Start()
-    {
-    }
+    void Start(){ }
 
     // Update is called once per frame
-    void Update()
-    {
-        //just trigger once, even if held (opposite of get key)
-        if (Input.GetKeyDown(KeyCode.Mouse0)){
+    public void OnFire(InputValue value){
+        if (value.isPressed){
             var clone = Instantiate(bullet);
             clone.transform.rotation = shootPoint.transform.rotation;
             clone.transform.position = shootPoint.transform.position;
-            
         }
     }
 }
